678778242b8327bc16f863d8ed577d93e2988f33
Mizhik <91588761+Mizhik@users.noreply.github.com>
2022-11-14 18:14:15 +0200

    Add files via upload

---
+++ PR2.6.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+void main() {
+    int c0, counter = 0;
+    std::cin >> c0;
+    do {
+        if (c0 % 2 == 0) c0 /= 2;
+        else c0 = 3 * c0 + 1;
+        std::cout << c0 << std::endl;
+        counter++;
+    } while (c0 != 1);
+    std::cout << "steps = " << counter << std::endl;
+}
