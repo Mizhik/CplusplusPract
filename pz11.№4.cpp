afc7e35a6653cade34cb71f033f6514bad165272
Mizhik <91588761+Mizhik@users.noreply.github.com>
2022-11-17 14:22:31 +0200

    Update pz11.№4.cpp

--- pz11.№4.cpp
+++ pz11.№4.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -13,9 +15,11 @@ int main() {
 	int A = Number();
 	int temp=0;
 	int a[5];
-	cout << "Enter array: ";
+	int min = 1;
+	int max = 30;
+	srand(time(NULL));
 	for (int i = 0; i < 5; i++) {
-		cin >> a[i];
+		a[i]= min + rand() % (max - min + 1);
 	}
 	for (int i = 0; i < 5; i++) {
 		if (A < a[i])
